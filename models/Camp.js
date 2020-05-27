bb47042e99d1919c423ef45b478c5db0ccd7ee08
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-05-27 15:49:15 +0700

    added slugify middlware

--- models/Camp.js
+++ models/Camp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const CampSchema = new mongoose.Schema({
   name: {
@@ -96,6 +97,11 @@ const CampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+// Create camp slug from name
+CampSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model('Camp', CampSchema);
