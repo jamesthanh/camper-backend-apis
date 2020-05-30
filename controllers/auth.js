cafd237d7890823af2d27dbf1cf476deb8840a17
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-05-30 14:02:02 +0700

    added password encryption

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,5 +6,14 @@ const User = require('../models/User');
 // @route GET /api/v1/camps
 // @access Public
 exports.register = aysncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
   res.status(200).json({ success: true });
 });
