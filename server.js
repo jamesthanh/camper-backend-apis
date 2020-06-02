5a6dd732ea2aba2804ad44b7a4581a55dc7c8cee
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-06-02 23:52:06 +0700

    added NoSQL injection

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const errorHanlder = require('./middleware/error');
 const connectDB = require('./config/db');
+const mongoSanitize = require('express-mongo-sanitize');
 
 // Load config file
 dotenv.config({ path: './config/config.env' });
@@ -36,6 +37,9 @@ if (process.env.NODE_ENV === 'development') {
 
 // File uploading
 app.use(fileupload());
+
+// Prevent NoSQL injection
+app.use(mongoSanitize());
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
