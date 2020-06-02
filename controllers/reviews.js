e3ca4de372802282ee4c9c879d8c6d5076e192e7
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-06-02 23:27:26 +0700

    added CRUD Review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -62,3 +62,52 @@ exports.addReview = aysncHandler(async (req, res, next) => {
     data: review,
   });
 });
+
+// @desc Update review
+// @route PUT /api/v1/reviews/:id
+// @access Private
+exports.updateReview = aysncHandler(async (req, res, next) => {
+  let review = await Review.findById(req.params.id);
+  if (!review) {
+    return next(
+      new ErrorResponse(`No Review found with the ID of ${req.params.id}`, 404)
+    );
+  }
+  // Check for the user owns the reviews for is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`Not authorize to update this review`, 401));
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: review,
+  });
+});
+
+// @desc Delete review
+// @route Delete /api/v1/reviews/:id
+// @access Private
+exports.deleteReview = aysncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+  if (!review) {
+    return next(
+      new ErrorResponse(`No Review found with the ID of ${req.params.id}`, 404)
+    );
+  }
+  // Check for the user owns the reviews for is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`Not authorize to update this review`, 401));
+  }
+
+  await review.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
