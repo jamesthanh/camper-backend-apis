2b8cadf8316ab5441afa9efc44cbd06c363cf1c7
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-06-02 23:40:58 +0700

    added logout user

--- controllers/auth.js
+++ controllers/auth.js
@@ -117,6 +117,20 @@ exports.resetPassword = aysncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+// @desc Log out + clear cookie
+// @route POST /api/v1/auth/logout
+// @access Private
+exports.logout = aysncHandler(async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
+
 // @desc Get current logged in user
 // @route POST /api/v1/auth/me
 // @access Private
--- middleware/auth.js
+++ middleware/auth.js
@@ -11,14 +11,13 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.token) {
+    token = req.cookies.token;
   }
-  //   else if (req.cookies.token) {
-  //       token = req.cookies.token
-  //   }
 
   // Check for sending token exists
   if (!token) {
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
   try {
     // Verify token
@@ -27,7 +26,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.user = await User.findById(decoded.id);
     next();
   } catch (err) {
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 });
 
