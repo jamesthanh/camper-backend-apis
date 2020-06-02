afc00c97ba2ee62145fe2ffaad51929bba4d4393
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-06-02 11:49:01 +0700

    added update user details and password

--- controllers/auth.js
+++ controllers/auth.js
@@ -128,6 +128,39 @@ exports.getMe = aysncHandler(async (req, res, next) => {
   });
 });
 
+// @desc Update password
+// @route POST /api/v1/auth/updatepassword
+// @access Private
+exports.updatePassword = aysncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+
+  // Check current password
+  if (!(await user.matchPassword(req.body.currentPassword))) {
+    return next(new ErrorResponse('Password is not valid', 401));
+  }
+  user.password = req.body.newPassword;
+  await user.save();
+  sendTokenResponse(user, 200, res);
+});
+
+// @desc Update User Detail
+// @route PUT /api/v1/auth/updatedetails
+// @access Private
+exports.updateDetails = aysncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
